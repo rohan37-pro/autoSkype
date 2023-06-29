25b186f7e468d21f7d95576606855a927f13235a
theAutomationGuy <65919389+rid23@users.noreply.github.com>
2023-06-29 14:24:09 +0530

    added human like intervals and delays , and a few element locators for robust performance

--- main.py
+++ main.py
@@ -14,6 +14,19 @@ import json
 
 ## this is to make it platform independent....
 options = webdriver.ChromeOptions()
+options.add_argument("--disable-notifications")
+
+#to disable the extensions
+options.add_argument("--disable-extensions")
+
+options.add_experimental_option("prefs", { \
+    "profile.default_content_setting_values.media_stream_mic": 1, 
+    "profile.default_content_setting_values.media_stream_camera": 1,
+    "profile.default_content_setting_values.geolocation": 1, 
+    "profile.default_content_setting_values.notifications": 1 
+  })
+
+
 if platform.system().lower() == 'linux':
     options.add_argument("--user-data-dir=cookies")
 elif platform.system().lower() == "windows" :
@@ -85,25 +98,64 @@ def dial_the_num(number ,code7digit):
         driver.find_element('xpath', "//button[@title='Audio Call']").click()
     except:
         driver.find_element('xpath', "//button[@title='Use dial pad']").click()
-        time.sleep(0.5)
-        ActionChains(driver).send_keys(number).send_keys(Keys.ENTER).perform()
-    
+        time.sleep(random.uniform(0.6 , 0.7))
+        #ActionChains(driver).send_keys(number).send_keys(Keys.ENTER).perform()
+        
+        pad = driver.find_element(By.XPATH ,'//div[@class = "inputGradient"]//input[@placeholder = "Phone number"]')
+        for i in number:
+            pad.send_keys(i)
+            time.sleep(random.uniform(0.2 , 0.8))
+        pad.send_keys(Keys.ENTER)
+
     # manual code for explicitly wait and check the button is clickable or not
     # waiting for 15 sec
+    random_wait = random.randint(3,5)
     for i in range(30):
         try:
-            driver.find_element('xpath', "//button[@title='Enter numbers using the dial pad']").click()
-            driver.find_element('xpath', "//button[@title='Close']")
-            input_box = driver.find_element('xpath', "//input[@type='text']")
-            # enter the 7 digit codes from text file
-            for i in range(3):
-                input_box.send_keys(code7digits[i].strip() + '#')
-                time.sleep(1)
-            input_box.send_keys('##')
+            try:
+                error_popup = driver.find_element(By.XPATH , '//div[@data-text-as-pseudo-element = "Oops, something went wrong."]')
+                message = serror_popup.get_attribute("data-text-as-pseudo-element")
+                print(message)
+                continue
+            except:
+                
             
+                try:
+                    element = WebDriverWait(driver, 15).until(EC.visibility_of_element_located((By.XPATH, '//div[@class="reactxp-ignore-pointer-events"]//button[@aria-label = "Captions"]')))
+                    print('The call has connected')
+                except:
+                    print("the call didin't go through")
+                    continue
+                finally:
+                    time.sleep(2)
+                    driver.find_element('xpath', "//button[@title='Enter numbers using the dial pad']").click()
+                    driver.find_element('xpath', "//button[@title='Close']")
+                    input_box = driver.find_element('xpath', "//input[@type='text']")
+                    #locating the hash key element on the dial pad for clicking using xpath
+                    the_hash_key = driver.find_element(By.XPATH , '//div[@data-text-as-pseudo-element = "#"]')
+                    # enter the 7 digit codes from text file
+                    time.sleep(5)
+                    count = 1
+                    for i in range(3):
+                        for char in code7digits[i].strip():
+                            print(f'the code being used is {char}')
+                            input_box.send_keys(char)
+                            time.sleep(random.uniform(0.5 , 0.6))
+                        time.sleep(0.4)
+                        the_hash_key.click()
+                        count+=1
+                        
+                        if count == 4:
+                            break
+                        time.sleep(6)
+                    
+                    for i in range(2):
+                        the_hash_key.click()
+                        time.sleep(random.uniform(0.4 , 0.8))
+                
         except:
             time.sleep(0.5)
-
+#the xpath for the error message popup text //div[@data-text-as-pseudo-element = "Oops, something went wrong."]
 # 7 digit code file handling....
 print("dialing the number...")
 with open('doc/phonenumber.txt','r') as file:
